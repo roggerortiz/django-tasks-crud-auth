7cfffd22c37a4c642790bee5bbebef9054a3a824
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2025-01-18 21:16:52 -0500

    database url

--- crud/settings.py
+++ crud/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import dj_database_url
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,13 +86,8 @@ WSGI_APPLICATION = "crud.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default="postgresql://postgres:postgres@localhost:5432/tasks",
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
     )
 }
